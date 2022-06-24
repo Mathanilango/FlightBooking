56f5600ac3b0d8d12c1b014cfa5e4348503e2e72
Mathanilango <108148826+Mathanilango@users.noreply.github.com>
2022-06-24 23:52:38 +0530

    Add files via upload

---
+++ Loginservice/Rabittmq.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketService.Model;
+
+namespace Loginservice
+{
+    public static class Rabittmq
+    {
+        public static void consume(IModel model)
+        {
+            model.QueueDeclare("Senddata", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            var consumer = new EventingBasicConsumer(model);
+            consumer.Received += (sender, e) =>
+            {
+                newtest tt = new newtest();
+                // tt=e.Body
+                var body = e.Body.ToArray();
+                var data = Encoding.UTF8.GetString(body);
+                //tt = data;
+            };
+            model.BasicConsume("Senddata", true, consumer);
+        }
+    }
+}
