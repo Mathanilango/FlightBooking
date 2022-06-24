64bd5d6ca4021d57a07e1a577cac0fbf7b00ee32
Mathanilango <108148826+Mathanilango@users.noreply.github.com>
2022-06-24 23:55:46 +0530

    Add files via upload

---
+++ Model/Ticket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Searchservice.Model
+{
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Pnrno { get; private set; }
+        public int FlightId { get; set; }
+        public int AirlineId { get; set; }
+        public string PassengerName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public int BookedSeats { get; set; }
+        public string Meal { get; set; }
+        public bool Removed { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime Dateofjourney { get; set; } = DateTime.Now;
+        public DateTime TicketcancelledDate { get; set; }
+    }
+}
